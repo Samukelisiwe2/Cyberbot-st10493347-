ada26d2667d606e78f04b34730999895d655b6c4
Samukelisiwe2 <ST10493347@rcconnect.edu.za>
2026-06-18 23:43:05 +0000

    Task Storage Helper (POE part3)

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cyberbot___st10493347_.Models;
 
 namespace Cyberbot___st10493347_.Data
 {
-    class ApplicationDbContext
-    {
+    public class ApplicationDbContext : DbContext
+        {
+            public DbSet<TaskItem> Tasks { get; set; } = null!;
+            public DbSet<ActivityLogEntry> Logs { get; set; } = null!;
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                if (!optionsBuilder.IsConfigured)
+                {
+                    optionsBuilder.UseSqlite("Data Source=database.db");
+                    optionsBuilder.UseLazyLoadingProxies();
+                }
+            }
+        }
     }
-}
+
+
+
+
--- Services/TaskStorageHelper.cs
+++ Services/TaskStorageHelper.cs
@@ -3,10 +3,105 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cyberbot___st10493347_.Data;
+using Cyberbot___st10493347_.Models;
 
 namespace Cyberbot___st10493347_.Services
 {
-    class TaskStorageHelper
-    {
+     public class TaskStorageHelper
+        {
+            public TaskStorageHelper()
+            {
+                using ApplicationDbContext db = new ApplicationDbContext();
+                db.Database.EnsureCreated();
+            }
+
+            public List<TaskItem> LoadTasks()
+            {
+                using ApplicationDbContext db = new ApplicationDbContext();
+                db.Database.EnsureCreated();
+
+                return db.Tasks
+                    .OrderByDescending(t => t.Id)
+                    .ToList();
+            }
+
+            public TaskItem AddTask(string title, string description, string reminder)
+            {
+                using ApplicationDbContext db = new ApplicationDbContext();
+                db.Database.EnsureCreated();
+
+                TaskItem task = new TaskItem
+                {
+                    Title = title,
+                    Description = description,
+                    Reminder = reminder,
+                    IsComplete = false,
+                    CreatedAt = DateTime.Now.ToString("dd MMM yyyy HH:mm")
+                };
+
+                db.Tasks.Add(task);
+                db.SaveChanges();
+
+                return task;
+            }
+
+            public bool MarkAsComplete(int id)
+            {
+                using ApplicationDbContext db = new ApplicationDbContext();
+                db.Database.EnsureCreated();
+
+                TaskItem? task = db.Tasks.FirstOrDefault(t => t.Id == id);
+
+                if (task == null)
+                {
+                    return false;
+                }
+
+                task.IsComplete = true;
+
+                db.Tasks.Update(task);
+                db.SaveChanges();
+
+                return true;
+            }
+
+            public bool UpdateReminder(int id, string reminder)
+            {
+                using ApplicationDbContext db = new ApplicationDbContext();
+                db.Database.EnsureCreated();
+
+                TaskItem? task = db.Tasks.FirstOrDefault(t => t.Id == id);
+
+                if (task == null)
+                {
+                    return false;
+                }
+
+                task.Reminder = reminder;
+
+                db.Tasks.Update(task);
+                db.SaveChanges();
+
+                return true;
+            }
+
+            public bool DeleteTask(int id)
+            {
+                using ApplicationDbContext db = new ApplicationDbContext();
+                db.Database.EnsureCreated();
+
+                TaskItem? task = db.Tasks.FirstOrDefault(t => t.Id == id);
+
+                if (task == null)
+                {
+                    return false;
+                }
+
+                db.Tasks.Remove(task);
+                db.SaveChanges();
+
+                return true;
+            }
+        }
     }
-}
