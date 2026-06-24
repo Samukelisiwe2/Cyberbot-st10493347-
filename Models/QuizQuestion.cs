a802a681bacf09cb95f337191e433608cc0ef90f
Samukelisiwe2 <ST10493347@rcconnect.edu.za>
2026-06-24 18:50:15 +0000

    updated code with comments

--- Models/QuizQuestion.cs
+++ Models/QuizQuestion.cs
@@ -8,10 +8,15 @@ namespace Cyberbot___st10493347_.Models
 {
         public class QuizQuestion
         {
-            public string Question { get; set; } = "";
-            public List<string> Options { get; set; } = new List<string>();
-            public string CorrectAnswer { get; set; } = "";
-            public string Explanation { get; set; } = "";
-            public bool IsTrueFalse { get; set; }
+        // The text of the quiz question.
+        public string Question { get; set; } = ""; 
+        // A list containing all possible answer options for the question.
+        public List<string> Options { get; set; } = new List<string>();
+        // The correct answer to the question
+        public string CorrectAnswer { get; set; } = "";
+        // Provides additional information explaining why the answer is correct.
+        public string Explanation { get; set; } = "";
+        // Indicates whether the question is a True/False question.
+        public bool IsTrueFalse { get; set; }
         }
     }
